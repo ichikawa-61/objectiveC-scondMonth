75c468639d3ec97d5c16cfd6e4922ea15ed4b2de
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-04-24 10:34:07 +0900

    モデルクラスの実装

--- HotPepperApp/HotPepperApp/Model/HotPepperApiManager.h
+++ HotPepperApp/HotPepperApp/Model/HotPepperApiManager.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface HotPepperApiManager : NSObject
+-(void)getShopInformation:(NSString*)area;
 
 @end
--- HotPepperApp/HotPepperApp/Model/HotPepperApiManager.m
+++ HotPepperApp/HotPepperApp/Model/HotPepperApiManager.m
@@ -7,7 +7,72 @@
 //
 
 #import "HotPepperApiManager.h"
+#import "ShopEntity.h"
+#import <AFNetworking/AFNetworking.h>
 
 @implementation HotPepperApiManager
+
+static NSString *const Url = @"https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?";
+
+NSString *const APIKey    = @"bc37ec7b3d44efe0";
+NSString *const APICount  = @"50";
+NSString *const APIFormat = @"json";
+
+
+NSString *const ShopId = @"id";
+NSString *const ShopName = @"name";
+NSString *const Access = @"access";
+NSString *const Opening = @"open";
+NSString *const Address = @"address";
+
+NSString *const FoodNamePath  = @"food.name";
+NSString *const AverageBudgetPath = @"budget.average";
+NSString *const GenreNamePath = @"genre.name";
+NSString *const LogoPath = @"photo.mobile.l";
+
+
+
+-(void)getShopInformation:(NSString*)area{
+
+//エンコード
+[area stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet alphanumericCharacterSet]];
+
+AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+[manager GET:Url
+  parameters:@{@"key" :APIKey,
+               @"count" : APICount,
+               @"keyword" :area,
+               @"format" :APIFormat}
+    progress:nil
+     success:^(NSURLSessionTask *task, id responseObject) {
+         
+         
+         NSDictionary* results = responseObject[@"results"];
+         NSDictionary* shopDict    = results[@"shop"];
+         
+         for (NSDictionary<NSString *, NSString *> *shop in shopDict){
+             ShopEntity *shopEntity = [[ShopEntity alloc]init];
+             shopEntity.shopId  = shop[ShopId];
+             shopEntity.name    = shop[ShopName];
+             shopEntity.open    = shop[Opening];
+             shopEntity.address = shop[Address];
+             shopEntity.access  = shop[Access];
+             shopEntity.food    = [shop valueForKeyPath:FoodNamePath];
+             shopEntity.averageBudget
+             = [shop valueForKeyPath:AverageBudgetPath];
+             shopEntity.genre   = [shop valueForKeyPath:GenreNamePath];
+             shopEntity.logo    = [shop valueForKeyPath: LogoPath];
+             
+             NSLog(@"%@",shopEntity.name);
+             NSLog(@"%@",shopEntity.averageBudget);
+             
+         }
+         
+         
+     } failure:^(NSURLSessionTask *operation, NSError *error) {
+         NSLog(@"エラー: %@", error);
+     }];
+
+}
 
 @end
