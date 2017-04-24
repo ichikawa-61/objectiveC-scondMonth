9ff11743eaa6bf6de4ea36baa9a3f4f342afba96
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-04-24 09:48:29 +0900

    first commit

---
+++ HotPepperApp/HotPepperApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotPepperApp
+//
+//  Created by Manami Ichikawa on 4/24/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotPepperApp/HotPepperApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HotPepperApp
+//
+//  Created by Manami Ichikawa on 4/24/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
